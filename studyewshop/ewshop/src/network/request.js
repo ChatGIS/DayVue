20080da9f8b3d4ac64cbecad3cad1af703a73dab
Dreamice <dreamice13@foxmail.com>
2022-01-03 23:07:51 +0800

    初始化项目封装网络请求

---
+++ studyewshop/ewshop/src/network/request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export function request(config){
+    const instance = axios.create({
+        baseURL: 'https://api.shop.eduwork.cn',
+        timeout: 5000
+    })
+
+    // 请求拦截
+    instance.interceptors.request.use(config=>{
+        // 如果有一个接口需要认证才可以访问，就在这里统一配置
+
+        // 直接放行
+        return config;
+    }, error => {
+
+    })
+    // 响应拦截
+    instance.interceptors.response.use(res=>{
+        return res;
+    }, error => {
+        // 如果需要授权才可以访问的接口，统一去login授权
+
+        // 如果有错误，就在这里面处理，显示错误信息
+
+    })
+}
--- studyewshop/ewshop/src/views/Home.vue
+++ studyewshop/ewshop/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="~assets/images/CHFLQQx675.jpg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="~assets/images/CHFLQQx675.jpg" style="width: 320px">-->
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from 'components/HelloWorld.vue'
+import {onMounted} from 'vue';
+import {getHomeAllData} from "network/home.js"
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  setup(){
+    onMounted(()=>{
+      console.log('11111111');
+      getHomeAllData().then(res => {
+        console.log(res);
+      }).catch(error=>{
+
+      })
+    })
   }
+
 }
 </script>
