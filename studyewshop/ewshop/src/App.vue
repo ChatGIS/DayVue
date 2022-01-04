652a2dc4dd2622007f36cbd2d5b8e94102fba882
Dreamice <dreamice13@foxmail.com>
2022-01-04 19:29:11 +0800

    app.vue添加底部导航菜单

--- studyewshop/ewshop/src/App.vue
+++ studyewshop/ewshop/src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
+  <div id="nav">
+    <router-link class="tab-bar-item" to="/">
+      <div class="icon"><i class="iconfont icon-shouye"></i></div>
+      <div>首页</div>
+    </router-link>
+    <router-link class="tab-bar-item" to="/category">
+      <div class="icon"><i class="iconfont icon-fenlei"></i></div>
+      <div>分类</div></router-link>
+    <router-link class="tab-bar-item" to="/shopcart">
+      <div class="icon"><i class="iconfont icon-gouwuche"></i></div>
+      <div>购物车</div></router-link>
+    <router-link class="tab-bar-item" to="/profile">
+      <div class="icon"><i class="iconfont icon-yonghu"></i></div>
+      <div>我的</div>
+    </router-link>
+  </div>
 </template>
 
 <style lang="scss">
 @import "assets/css/base.css";
+@import "assets/css/iconfont.css";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -18,15 +31,36 @@
 }
 
 #nav {
-  padding: 30px;
+  background-color: #F6F6F6;
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -3px 1px rgba(100, 100, 100, 0.1);
+  //padding: 30px;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
+    //font-weight: bold;
+    //color: #2c3e50;
+    color: var(--color-text);
     &.router-link-exact-active {
       color: var(--color-tint);  /* 调整为调用样式文件中的变量颜色 */
     }
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 50px;
+    font-size: var(--font-size);
+  }
+  .tab-bar-item .icon {
+    width: 24px;
+    height: 24px;
+    margin-top: 5px;
+    vertical-align: middle;
+    display: inline-block;
   }
 }
 </style>
