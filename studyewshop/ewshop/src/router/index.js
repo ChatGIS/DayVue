6de70926b90f42f037064afd8fd96ad54b0f10f3
Dreamice <dreamice13@foxmail.com>
2022-01-04 19:15:34 +0800

    添加5个页面的路由

--- studyewshop/ewshop/src/router/index.js
+++ studyewshop/ewshop/src/router/index.js
@@ -1,20 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+const Home = () => import('../views/home/Home');
+const Category = () => import('../views/category/Category');
+const Detail = () => import('../views/detail/Detail');
+const Profile = () => import('../views/profile/Profile');
+const ShopCart = () => import('../views/shopcart/ShopCart');
 
 const routes = [
+  // 默认路由
   {
-    path: '/',
+    path: '',
+    name: 'DefaultHome',
+    component: Home
+  },
+  //  首页
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/category',
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail
+  },
+  {
+    path: '/shopcart',
+    name: 'ShopCart',
+    component: ShopCart
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
