53f7ec517b4b4deceee23436676ca741529ef48d
Dreamice <dreamice13@foxmail.com>
2022-01-06 21:30:08 +0800

    添加首页推荐商品，并实现跳转到详情

--- studyewshop/ewshop/src/views/home/Home.vue
+++ studyewshop/ewshop/src/views/home/Home.vue
@@ -4,18 +4,46 @@
       <template v-slot:default>图书兄弟</template>
     </nav-bar>
   </div>
+  
+  <div class="banners">
+    <img src="~assets/images/CHFLQQx675.jpg" alt="">
+  </div>
+
+  <recommend-view :recommends="recommends"></recommend-view>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
+  import RecommendView from "./ChildComps/RecommendView";
+  import {getHomeAllData} from "network/home";
+  import {ref, reactive,onMounted} from "vue";
+
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      RecommendView
+    },
+    setup() {
+      const recommends = ref([]);
+
+      onMounted(() => {
+        getHomeAllData().then(res => {
+          recommends.value = res.data.goods.data;
+        })
+      })
+
+      return {
+        recommends
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  .banners img {
+    width: 100%;
+    height: auto;
+    margin-top: 45px;
+  }
 </style>
