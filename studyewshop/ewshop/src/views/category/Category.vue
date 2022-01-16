8bc0ee0e83c13f732442ba2370c95f454d3878ff
Dreamice <dreamice13@foxmail.com>
2022-01-16 22:19:28 +0800

    分类页面布局和左侧菜单-使用vant手风琴折叠面板和sidebar侧边导航

--- studyewshop/ewshop/src/views/category/Category.vue
+++ studyewshop/ewshop/src/views/category/Category.vue
@@ -3,19 +3,91 @@
     <nav-bar>
       <template v-slot:default>商品分类</template>
     </nav-bar>
+	
+	<div id="mainbox">
+		<div class="ordertab">
+			
+		</div>
+
+		<van-sidebar class='leftmenu' v-model="activeKey">
+			<van-collapse v-model="activeName" accordion>
+			  <van-collapse-item v-for="(item,index) in categories" 
+				:key="item.id"
+				:title="item.name" 
+				:name="item.name">
+				<van-sidebar-item v-for="sub in item.children" 
+				:title="sub.name" />
+			  </van-collapse-item>
+			</van-collapse>
+		</van-sidebar>
+
+		<div class="goodslist">
+			
+		</div>
+	</div>
   </div>
 </template>
 
 <script>
-  import NavBar from "../../components/common/navbar/NavBar";
+  import NavBar from "components/common/navbar/NavBar";
+  import {getCategory} from "network/category";
+  import {ref, reactive, onMounted} from 'vue';
+  
   export default {
     name: "Category",
     components: {
       NavBar
-    }
+    },
+	setup() {
+	  let activeKey = ref(0);
+	  let categories = ref([]);
+	  let activeName = ref('1');
+	  
+	  onMounted(() => {
+		  getCategory().then(res => {
+			  categories.value = res.data.categories;
+		  })
+	  })
+	  
+	  return {
+		  activeKey,
+		  categories,
+		  activeName 
+	  }
+	}
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+	#mainbox {
+		margin-top: 45px;
+		display: flex;
+		.ordertab {
+			flex: 1;
+			float: right;
+			width: 100%;
+			height: 50px;
+			z-index: 9px;
+			position: fixed;
+			top: 45px;
+			right: 0;
+			left: 130px;
+		}
+		.leftmenu {
+			position: fixed;
+			top: 95px;
+			left: 0;
+			width: 130px;
+			width: 130px;
+		}
+		.goodslist {
+			flex: 1;
+			height: 200px;
+			position: absolute;
+			top: 100px;
+			left: 130px;
+			right: 0;
+			height: 100vh;
+		}
+	}
 </style>
