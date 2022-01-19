70430772332d998782c9d54dcfa8f0980a4ec740
Dreamice <dreamice13@foxmail.com>
2022-01-19 19:06:21 +0800

    登录验证-token-重要

--- studyewshop/ewshop/src/network/request.js
+++ studyewshop/ewshop/src/network/request.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { Notify } from 'vant';
+import {Notify, Toast} from 'vant';
+import router from '../router'
 
 export function request(config){
     const instance = axios.create({
@@ -10,7 +11,10 @@ export function request(config){
     // 请求拦截
     instance.interceptors.request.use(config => {
         // 如果有一个接口需要认证才可以访问，就在这里统一配置
-
+        const token = window.localStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = 'Bearer ' + token;
+        }
         // 直接放行
         return config;
     }, error => {
@@ -18,10 +22,16 @@ export function request(config){
     })
     // 响应拦截
     instance.interceptors.response.use(res=>{
+        // return res.data? res.data : res;
         return res;
     }, error => {
         // 如果需要授权才可以访问的接口，统一去login授权
-
+        if(error.response.status == '401'){
+            Toast.fail('请先登录');
+            router.push({
+                path: '/login'
+            })
+        }
         // 如果有错误，就在这里面处理，显示错误信息
         Notify({ type: 'warning', message: error.response.data.errors[Object.keys(error.response.data.errors)[0]][0] });
     })
--- studyewshop/ewshop/src/views/profile/Profile.vue
+++ studyewshop/ewshop/src/views/profile/Profile.vue
@@ -3,15 +3,45 @@
     <nav-bar>
       <template v-slot:default>个人中心</template>
     </nav-bar>
+
+    <div style="margin:15px;margin-top:100px">
+      <van-button @click="tologout" round block color="#43ba80">退出</van-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import NavBar from "../../components/common/navbar/NavBar";
+  import NavBar from "components/common/navbar/NavBar";
+  import {logout} from "network/user";
+  import { Toast  } from 'vant';
+  import {useRouter} from "vue-router";
+
   export default {
     name: "Profile",
     components: {
       NavBar
+    },
+    setup() {
+      const router = useRouter();
+
+      const tologout = () => {
+        logout().then(res => {
+          Toast.success('退出成功');
+
+          // 清除token
+          window.localStorage.setItem('token', '')
+
+          setTimeout(() => {
+            router.push({
+              path:'/login'
+            })
+          }, 500)
+        })
+      }
+
+      return {
+        tologout
+      }
     }
   }
 </script>
