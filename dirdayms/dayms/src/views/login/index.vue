0d66e7718d791965a2174c3cefbe873f44070192
Dreamice <dreamice13@foxmail.com>
2022-02-13 10:08:55 +0800

    dayms初始化-用户登录页面样式调整

--- dirdayms/dayms/src/views/login/index.vue
+++ dirdayms/dayms/src/views/login/index.vue
@@ -4,18 +4,19 @@
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
-      <el-icon :size="20" class="svg-container">
-        <edit />
-      </el-icon>
-      <el-form-item label="Activity name">
+      <el-form-item>
+        <el-icon :size="20" class="svg-container">
+          <edit />
+        </el-icon>
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-icon :size="20" class="svg-container">
-        <edit />
-      </el-icon>
-      <el-form-item label="Activity name">
+      <el-form-item>
+        <el-icon :size="20" class="svg-container">
+          <edit />
+        </el-icon>
         <el-input v-model="form.password"></el-input>
       </el-form-item>
+      <el-button type="primary" class="login-button">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -58,6 +59,78 @@
         border-radius: 5px;
         color: #454545;
       }
+
+      ::v-deep .el-input {
+        display: inline-block;
+        height: 47px;
+        width: 85%;
+
+        input {
+          background: transparent;
+          border: 0px;
+          -webkit-appearance: none;
+          border-radius: 0px;
+          padding: 12px 5px 12px 15px;
+          color: $light-gray;
+          height: 47px;
+          caret-color: $cursor;
+        }
+      }
+
+      .login-button {
+        width: 100%;
+        box-sizing: border-box;
+      }
+    }
+
+    .tips {
+      font-size: 16px;
+      line-height: 28px;
+      color: #fff;
+      margin-bottom: 10px;
+
+      span {
+        &:first-of-type {
+          margin-right: 16px;
+        }
+      }
+    }
+
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark-gray;
+      vertical-align: middle;
+      display: inline-block;
+    }
+
+    .title-container {
+      position: relative;
+
+      .title {
+        font-size: 26px;
+        color: $light-gray;
+        margin: 0px auto 40px auto;
+        text-align: center;
+        font-weight: bold;
+      }
+
+      ::v-deep .lang-select {
+        position: absolute;
+        top: 4px;
+        right: 0;
+        background-color: white;
+        font-size: 22px;
+        padding: 4px;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+    }
+
+    .show-pwd {
+      font-size: 16px;
+      color: $dark-gray;
+      cursor: pointer;
+      user-select: none;
     }
   }
 </style>
