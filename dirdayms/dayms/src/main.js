6b97f3091ea6c319f3e0e75ebf0e781ff43990d6
Dreamice <dreamice13@foxmail.com>
2022-02-20 21:52:47 +0800

    daymsHeader-导入i18n中英资源，面包屑导航使用

--- dirdayms/dayms/src/main.js
+++ dirdayms/dayms/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.scss'
+import i18n from '@/i18n'
 import '@/router/permission'
 import * as ELIcons from '@element-plus/icons-vue'
 import filters from "./utils/filters";
@@ -14,4 +15,4 @@ for(const iconName in ELIcons) {
 }
 // 时间过滤
 filters(app)
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(i18n).mount('#app')
