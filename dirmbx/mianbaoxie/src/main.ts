56cbf9b23fe092476d94e4d8a8593b89a632518e
Dreamice <dreamice13@foxmail.com>
2022-04-19 19:33:58 +0800

    mbx-项目初始化-添加样式文件css

--- dirmbx/mianbaoxie/src/main.ts
+++ dirmbx/mianbaoxie/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
+import './assets/styles/index.css'
 
 // 创建pinia实例
 const pinia = createPinia()
