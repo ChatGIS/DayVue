840ec0f027fbb4cd2cd854028df5e509b921a8e3
Dreamice <dreamice13@foxmail.com>
2022-11-30 19:16:44 +0800

    响应式基础-ref

--- official-example/src/components/ReactivityFundamentals.vue
+++ official-example/src/components/ReactivityFundamentals.vue
@@ -1,11 +1,19 @@
 <script setup lang='ts'>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const state = reactive({ count: 0 })
+const count = ref(0)
+// const count1 = $ref(0)
 
 const increment = () => {
     state.count++
 }
+ const consoleLog = () => {
+    console.log(count)
+    console.log(count.value)
+    count.value++
+    console.log(count.value)
+ }
 </script>
 
 <template>
@@ -14,7 +22,9 @@ const increment = () => {
         <h2>声明响应式状态</h2>
         <button @click="increment">{{ state.count }}</button>
         <h2>用ref()定义响应式变量</h2>
-        
+        <button @click="consoleLog">打印ref的值</button>
+        <br>
+        <span>ref在模板中自动解包，不需要.value; {{ count }}</span>
     </div>
 </template>
 
